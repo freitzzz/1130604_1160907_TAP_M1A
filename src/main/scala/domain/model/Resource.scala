d2e035b7dd9dd1a75ceffad7efdc098c72222d16
João Freitas <1160907@isep.ipp.pt>
2020-04-18 19:00:05 +0100

    Moved Teacher and External to Resource.scala and made Resource class sealed in order to seal the Resource contract, making the domain more consistent. Added smart constructors with proper validations for both Teacher and External classes

--- src/main/scala/domain/model/Resource.scala
+++ src/main/scala/domain/model/Resource.scala
@@ -1,6 +1,58 @@
 package domain.model
 
-abstract class Resource(id: String,
-                        name: String,
-                        availabilities: List[Availability],
-                        roles: List[Role])
+sealed abstract class Resource(id: String,
+                               name: String,
+                               availabilities: List[Availability],
+                               roles: List[Role]) {}
+
+object Resource {
+
+  def validResource(id: String,
+                    name: String,
+                    availabilities: List[Availability],
+                    roles: List[Role]): Boolean = {
+    availabilities.distinct.size == availabilities.size && roles.nonEmpty
+  }
+
+}
+
+case class Teacher private (id: String,
+                            name: String,
+                            availabilities: List[Availability],
+                            roles: List[Role])
+    extends Resource(id, name, availabilities, roles)
+
+object Teacher {
+  def create(id: String,
+             name: String,
+             availabilities: List[Availability],
+             roles: List[Role]): Option[Teacher] = {
+    if (Resource.validResource(id, name, availabilities, roles)
+        && !roles.exists((role) => role.getClass == Supervisor.getClass))
+      Some(new Teacher(id, name, availabilities, roles))
+    else
+      None
+  }
+}
+
+case class External private (id: String,
+                             name: String,
+                             availabilities: List[Availability],
+                             roles: List[Role])
+    extends Resource(id, name, availabilities, roles)
+
+object External {
+  def create(id: String,
+             name: String,
+             availabilities: List[Availability],
+             roles: List[Role]): Option[External] = {
+    if (Resource.validResource(id, name, availabilities, roles)
+        && !roles.exists(
+          (role) =>
+            role.getClass == President.getClass || role.getClass == Adviser.getClass
+        ))
+      Some(new External(id, name, availabilities, roles))
+    else
+      None
+  }
+}
--- src/main/scala/domain/model/Teacher.scala
+++
@@ -1,7 +0,0 @@
-package domain.model
-
-class Teacher(id: String,
-              name: String,
-              availabilities: List[Availability],
-              roles: List[Role])
-    extends Resource(id, name, availabilities, roles) {}
