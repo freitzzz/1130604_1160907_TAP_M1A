5cd5b9b406465edf1095b1c39d47344c6a12f6ad
Gustavo Moreira <1130604@isep.ipp.pt>
2020-04-19 19:30:09 +0100

    Further attempt to successfully load the file

--- src/main/scala/startup/Main.scala
+++ src/main/scala/startup/Main.scala
@@ -22,114 +22,158 @@ object Main {
 
     val agenda = fileLoader.get
 
+    // Retrieve Vivas
+    val vivasXML = agenda \ "vivas" \ "viva"
+
+    val roles = vivasXML
+      .map(viva => viva.child)
+      .flatten
+      .filter(node => node.label != "#PCDATA")
+      .map(
+        node =>
+          (node.attributes("id").toString(), node.label match {
+            case "president"  => Some(President())
+            case "adviser"    => Some(Adviser())
+            case "coadviser"  => Some(CoAdviser())
+            case "supervisor" => Some(Supervisor())
+            case _            => None
+          })
+      )
+
+    val test = roles
+      .groupBy(role => role._1)
+      .map(role => role._2)
+      .flatten
+      .map(role => role._2)
+    println(test)
+
+    val mappedRoles = roles.groupMap(key => key._1)(
+      value =>
+        roles
+          .filter(role => role._1 == value._1)
+          .map(x => x._2.get)
+    )
+
+    println(mappedRoles)
+
     // Retrieve teachers
     val teachersXML = agenda \ "resources" \ "teachers" \ "teacher"
     val mappedTeachers = teachersXML
-      .map(
-        node =>
-          Teacher(
-            node.attributes("id").toString(),
-            node.attributes("name").toString(),
-            (node \ "availability")
-              .map(
-                childNode =>
-                  Availability
-                    .create(
-                      LocalDateTime
-                        .parse(childNode.attributes("start").toString()),
-                      LocalDateTime
-                        .parse(childNode.attributes("end").toString()),
-                      childNode.attributes("preference").toString().toInt
-                    )
-                    .get
-              )
-              .toList,
-            roles = List[Role]()
-        )
+      .groupMap(keyNode => keyNode.attributes("id").toString())(
+        valueNode =>
+          Teacher
+            .create(
+              valueNode.attributes("id").toString(),
+              valueNode.attributes("name").toString(),
+              (valueNode \ "availability")
+                .map(
+                  childNode =>
+                    Availability
+                      .create(
+                        LocalDateTime
+                          .parse(childNode.attributes("start").toString()),
+                        LocalDateTime
+                          .parse(childNode.attributes("end").toString()),
+                        childNode.attributes("preference").toString().toInt
+                      )
+                      .get
+                )
+                .toList,
+              roles = List[Role]()
+          )
       )
-      .toList
+
+    println(mappedTeachers)
 
     // Retrieve externals
     val externalsXML = agenda \ "resources" \ "externals" \ "external"
     val mappedExternals = externalsXML
       .map(
         node =>
-          Teacher(
-            node.attributes("id").toString(),
-            node.attributes("name").toString(),
-            (node \ "availability")
-              .map(
-                childNode =>
-                  Availability
-                    .create(
-                      LocalDateTime
-                        .parse(childNode.attributes("start").toString()),
-                      LocalDateTime
-                        .parse(childNode.attributes("end").toString()),
-                      childNode.attributes("preference").toString().toInt
-                    )
-                    .get
-              )
-              .toList,
-            roles = List[Role]()
-        )
+          External
+            .create(
+              node.attributes("id").toString(),
+              node.attributes("name").toString(),
+              (node \ "availability")
+                .map(
+                  childNode =>
+                    Availability
+                      .create(
+                        LocalDateTime
+                          .parse(childNode.attributes("start").toString()),
+                        LocalDateTime
+                          .parse(childNode.attributes("end").toString()),
+                        childNode.attributes("preference").toString().toInt
+                      )
+                      .get
+                )
+                .toList,
+              roles = List[Role]()
+            )
+            .get
       )
       .toList
 
-    // Retrieve Vivas
-    val vivasXML = agenda \ "vivas" \ "viva"
+    //
+
     println(vivasXML)
     val xxx = vivasXML \ "president"
     print(xxx)
-    val vivas = vivasXML
-      .map(
-        node =>
-          Viva(
-            node.attributes("student").toString(),
-            node.attributes("title").toString(),
-            jury = Jury
-              .create(
-                president = mappedTeachers
-                  .find(
-                    p =>
-                      p.id == (node \ "president")
-                        .map(childNode => childNode.attributes("id").toString())
-                        .head
-                  )
-                  .get,
-                adviser = mappedTeachers
-                  .find(
-                    a =>
-                      a.id == (node \ "adviser")
-                        .map(childNode => childNode.attributes("id").toString())
-                        .head
-                  )
-                  .get,
-                supervisors = (node \ "supervisor")
-                  .map(
-                    childNode =>
-                      mappedExternals
-                        .find(
-                          s => s.id == childNode.attributes("id").toString()
-                        )
-                        .get
-                  )
-                  .toList,
-                coAdvisers = (node \ "coadviser")
-                  .map(
-                    childNode =>
-                      mappedExternals
-                        .find(
-                          s => s.id == childNode.attributes("id").toString()
-                        )
-                        .get
-                  )
-                  .toList
-              )
-              .get
-        )
-      )
-      .toList
+//    val vivas = vivasXML
+//      .map(
+//        node =>
+//          Viva
+//            .create(
+//              node.attributes("student").toString(),
+//              node.attributes("title").toString(),
+//              Jury
+//                .create(
+//                  president = mappedTeachers
+//                    .find(
+//                      p =>
+//                        p.id == (node \ "president")
+//                          .map(
+//                            childNode => childNode.attributes("id").toString()
+//                          )
+//                          .head
+//                    )
+//                    .get,
+//                  adviser = mappedTeachers
+//                    .find(
+//                      a =>
+//                        a.id == (node \ "adviser")
+//                          .map(
+//                            childNode => childNode.attributes("id").toString()
+//                          )
+//                          .head
+//                    )
+//                    .get,
+//                  supervisors = (node \ "supervisor")
+//                    .map(
+//                      childNode =>
+//                        mappedExternals
+//                          .find(
+//                            s => s.id == childNode.attributes("id").toString()
+//                          )
+//                          .get
+//                    )
+//                    .toList,
+//                  coAdvisers = (node \ "coadviser")
+//                    .map(
+//                      childNode =>
+//                        mappedExternals
+//                          .find(
+//                            s => s.id == childNode.attributes("id").toString()
+//                          )
+//                          .get
+//                    )
+//                    .toList
+//                )
+//                .get
+//            )
+//            .get
+//      )
+//      .toList
 
     val x = 2
 
