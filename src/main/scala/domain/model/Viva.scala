b136823ec2a41ac12005ac1cc43790405eb29151
João Freitas <1160907@isep.ipp.pt>
2020-04-26 15:13:12 +0100

    Viva now has duration

--- src/main/scala/domain/model/Viva.scala
+++ src/main/scala/domain/model/Viva.scala
@@ -1,18 +1,22 @@
 package domain.model
 
+import java.time.Duration
+
 import scala.util._
 
 sealed abstract case class Viva private (student: NonEmptyString,
                                          title: NonEmptyString,
-                                         jury: Jury)
+                                         jury: Jury,
+                                         duration: Duration)
 
 object Viva {
 
   def create(student: NonEmptyString,
              title: NonEmptyString,
-             jury: Jury): Try[Viva] = {
+             jury: Jury,
+             duration: Duration): Try[Viva] = {
     // no validations so far to check but necessary to comply with the ADT and Smart Constructors pattern
-    Success(new Viva(student, title, jury) {})
+    Success(new Viva(student, title, jury, duration) {})
   }
 
 }
