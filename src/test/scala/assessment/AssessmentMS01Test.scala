3290df080f6b3759561cb660e1767315f7acc5f6
Gustavo Moreira <1130604@isep.ipp.pt>
2020-04-29 20:01:37 +0100

    removed main and start running from tests

--- src/test/scala/assessment/AssessmentMS01Test.scala
+++ src/test/scala/assessment/AssessmentMS01Test.scala
@@ -2,10 +2,14 @@ package assessment
 
 import org.scalatest.funsuite.AnyFunSuite
 import assessment.AssessmentMS01.create
+import io.FileIO
+import xml.Functions
+
+import scala.util.{Failure, Success}
 
 class AssessmentMS01Test extends AnyFunSuite with AssessmentBehaviours {
 
-  val PATH = "files/assessment/ms01"   // Assessment file path
+  val PATH = "files/assessment/ms01" // Assessment file path
 
   performTests(create)
 }
