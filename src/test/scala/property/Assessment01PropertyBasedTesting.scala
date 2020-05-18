5f214e0cc04d26c01dad1724e59453c8a0186306
João Freitas <1160907@isep.ipp.pt>
2020-05-18 23:04:13 +0100

    Progress on pbt

--- src/test/scala/property/Assessment01PropertyBasedTesting.scala
+++ src/test/scala/property/Assessment01PropertyBasedTesting.scala
@@ -85,101 +85,56 @@ class Assessment01PropertyBasedTesting extends Properties("") {
     p <- Gen.oneOf(randomLocalDateTimes)
   } yield Period.create(p.head, p.last).get
 
-  val genValidPreference: Gen[Preference] = for {
+  val genPreference: Gen[Preference] = for {
     i <- Gen.chooseNum(1, 5)
   } yield Preference.create(i).get
 
-  val genInvalidPreference: Gen[Preference] = for {
-    i <- Gen.chooseNum(6, 900)
-  } yield Preference.create(i).get
-
-  val genValidAvailability: Gen[Availability] = for {
+  val genAvailability: Gen[Availability] = for {
     period <- genPeriod
-    preference <- genValidPreference
-  } yield Availability.create(period, preference)
-
-  val genInvalidAvailability: Gen[Availability] = for {
-    period <- genPeriod
-    preference <- genInvalidPreference
+    preference <- genPreference
   } yield Availability.create(period, preference)
 
   val genRole: Gen[Role] = for {
-    r <- Gen.oneOf(Set(President, Adviser, CoAdviser, Supervisor))
-  } yield r.apply()
+    r <- Gen.oneOf(Set(President(), Adviser(), CoAdviser(), Supervisor()))
+  } yield r
 
   val genTeacherRole: Gen[Role] = for {
-    r <- Gen.oneOf(Set(President, Adviser))
-  } yield r.apply()
+    r <- Gen.oneOf(Set(President(), Adviser(), CoAdviser()))
+  } yield r
 
   val genExternalRole: Gen[Role] = for {
-    r <- Gen.oneOf(Set(Supervisor, CoAdviser))
-  } yield r.apply()
-
-  val genPresidentRole: Gen[Role] = for {
-    p <- Gen.oneOf(Set(President))
-  } yield p.apply()
-
-  val genAdviserRole: Gen[Role] = for {
-    p <- Gen.oneOf(Set(Adviser))
-  } yield p.apply()
-
-  val genSupervisorRole: Gen[Role] = for {
-    p <- Gen.oneOf(Set(Supervisor))
-  } yield p.apply()
-
-  val genCoAdviserRole: Gen[Role] = for {
-    p <- Gen.oneOf(Set(CoAdviser))
-  } yield p.apply()
+    r <- Gen.oneOf(Set(Supervisor(), CoAdviser()))
+  } yield r
 
   val genPresidentResource: Gen[Resource] = for {
     id <- genNonEmptyString
     name <- genNonEmptyString
-    availabilities <- Gen.listOf(genValidAvailability)
-    roles <- Gen.listOf(genPresidentRole)
-  } yield
-    Resource
-      .validResource(id, name, availabilities, roles)
-      .get
-      .asInstanceOf[Resource]
+    availabilities <- Gen.listOf(genAvailability)
+  } yield Teacher.create(id, name, availabilities, List(President())).get
 
   val genAdviserResource: Gen[Resource] = for {
     id <- genNonEmptyString
     name <- genNonEmptyString
-    availabilities <- Gen.listOf(genValidAvailability)
-    roles <- Gen.listOf(genAdviserRole)
-  } yield
-    Resource
-      .validResource(id, name, availabilities, roles)
-      .get
-      .asInstanceOf[Resource]
+    availabilities <- Gen.listOf(genAvailability)
+  } yield Teacher.create(id, name, availabilities, List(Adviser())).get
 
   val genSupervisorResource: Gen[Resource] = for {
     id <- genNonEmptyString
     name <- genNonEmptyString
-    availabilities <- Gen.listOf(genValidAvailability)
-    roles <- Gen.listOf(genSupervisorRole)
-  } yield
-    Resource
-      .validResource(id, name, availabilities, roles)
-      .get
-      .asInstanceOf[Resource]
+    availabilities <- Gen.listOf(genAvailability)
+  } yield Teacher.create(id, name, availabilities, List(Supervisor())).get
 
   val genCoAdviserResource: Gen[Resource] = for {
     id <- genNonEmptyString
     name <- genNonEmptyString
-    availabilities <- Gen.listOf(genValidAvailability)
-    roles <- Gen.listOf(genCoAdviserRole)
-  } yield
-    Resource
-      .validResource(id, name, availabilities, roles)
-      .get
-      .asInstanceOf[Resource]
+    availabilities <- Gen.listOf(genAvailability)
+  } yield External.create(id, name, availabilities, List(CoAdviser())).get
 
-  val genValidTeacher: Gen[Teacher] = for {
+  val genTeacher: Gen[Teacher] = for {
     id <- genNonEmptyString
     name <- genNonEmptyString
-    availabilities <- Gen.listOfN(2, genValidAvailability)
-    roles <- Gen.listOfN(2, genTeacherRole)
+    availabilities <- Gen.listOf(genAvailability)
+    roles <- Gen.pick(2, Gen.listOf(genTeacherRole))
   } yield Teacher.create(id, name, availabilities, roles).get
 
   val genValidExternal: Gen[External] = for {
