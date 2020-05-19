0ca4a39efc823e2ee06c5008ac601e565ed9bc2e
João Freitas <1160907@isep.ipp.pt>
2020-05-19 17:19:48 +0100

    Added property based testings for Preference class creation

---
+++ src/test/scala/property/PreferencePropertyBasedTesting.scala
@@ -0,0 +1,24 @@
+package property
+
+import domain.model.Preference
+import org.scalacheck.Prop._
+import org.scalacheck.{Gen, Properties}
+
+object PreferencePropertyBasedTesting extends Properties("preference") {
+
+  property("preference cannot assume values lower than 1") =
+    forAll(Gen.chooseNum(Int.MinValue, 0)) { (value: Int) =>
+      Preference.create(value).isFailure
+    }
+
+  property("preference cannot assume values higher than 5") =
+    forAll(Gen.chooseNum(6, Int.MaxValue)) { (value: Int) =>
+      Preference.create(value).isFailure
+    }
+
+  property("preference can assume values in range 1-5") =
+    forAll(Gen.chooseNum(1, 5)) { (value: Int) =>
+      Preference.create(value).isSuccess
+    }
+
+}
