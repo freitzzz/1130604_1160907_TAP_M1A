ef4a34b0b267e663c104a4bbcc51c0c85456a12b
João Freitas <1160907@isep.ipp.pt>
2020-05-19 20:26:58 +0100

    Added Duration property based tests

---
+++ src/test/scala/property/DurationPropertyBasedTesting.scala
@@ -0,0 +1,23 @@
+package property
+
+import domain.model.Duration
+import org.scalacheck.Prop._
+import org.scalacheck.{Gen, Properties}
+
+object DurationPropertyBasedTesting extends Properties("duration") {
+
+  property("duration cannot assume negative values") = {
+    forAll(Generators.genNegativeJavaTimeDuration) {
+      duration: java.time.Duration =>
+        Duration.create(duration).isFailure
+    }
+  }
+
+  property("duration can assume positive values") = {
+    forAll(Generators.genGreaterThanZeroJavaTimeDuration) {
+      duration: java.time.Duration =>
+        Duration.create(duration).isSuccess
+    }
+  }
+
+}
