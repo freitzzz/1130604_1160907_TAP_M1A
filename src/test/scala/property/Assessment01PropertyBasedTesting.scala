184ab24ef2f23ca312bb961ea869ef99c70732cd
João Freitas <1160907@isep.ipp.pt>
2020-05-23 18:53:48 +0100

    Added property that verify that after schedule vivas update, all vivas resources should have different availabilities, except the first schedule viva and a property that verifies that vivas scheduled should be in a First Come First Serve order

--- src/test/scala/property/Assessment01PropertyBasedTesting.scala
+++ src/test/scala/property/Assessment01PropertyBasedTesting.scala
@@ -605,6 +605,105 @@ object Assessment01PropertyBasedTesting extends Properties("") {
     }
   }
 
+  property("vivas scheduled should be in a First Come First Serve order") = {
+    val vivasDuration = for {
+      durationPeriod <- Generators.genAtMost24HPeriodOfTime
+      duration <- Duration.create(
+        java.time.Duration.between(durationPeriod._1, durationPeriod._2)
+      )
+    } yield duration
+
+    val dateTimeNow = LocalDateTime.now()
+
+    val vivasToScheduleGenerator = for {
+      duration <- vivasDuration
+      availabilities <- Generators.genAvailabilitySequenceOf(
+        10,
+        dateTimeNow,
+        duration.get.timeDuration
+      )
+      presidents <- Generators.genResourcesWith(
+        availabilities,
+        List(President()),
+        2
+      )
+      advisers <- Generators.genResourcesWith(
+        availabilities,
+        List(Adviser()),
+        2
+      )
+      coAdvisersLength <- Gen.chooseNum(
+        0,
+        Math.min(presidents.length, advisers.length)
+      )
+      supervisorsLength <- Gen.chooseNum(
+        0,
+        Math.min(presidents.length, advisers.length)
+      )
+      coAdvisers <- Gen.listOfN(
+        coAdvisersLength,
+        Generators.genResourcesWith(availabilities, List(CoAdviser()), 0)
+      )
+      supervisors <- Gen.listOfN(
+        supervisorsLength,
+        Generators.genResourcesWith(availabilities, List(Supervisor()), 0)
+      )
+    } yield (duration.get, presidents, advisers, coAdvisers, supervisors)
+
+    Prop.forAll(
+      vivasToScheduleGenerator,
+      Generators.genName,
+      Generators.genName
+    ) { (arguments, student, title) =>
+      {
+
+        val vivasDuration = arguments._1
+
+        val presidents = arguments._2
+
+        val advisers = arguments._3
+
+        val coAdvisers = arguments._4
+
+        val supervisors = arguments._5
+
+        val lessResourcesGeneratedLength =
+          Math.min(presidents.length, advisers.length)
+
+        val resources = (0 until lessResourcesGeneratedLength).map(
+          index =>
+            (
+              presidents(index),
+              advisers(index),
+              supervisors.lift(index).getOrElse(List()),
+              coAdvisers.lift(index).getOrElse(List())
+          )
+        )
+
+        val vivas = resources.map(
+          pair =>
+            Viva.create(
+              student,
+              title,
+              Jury.create(pair._1, pair._2, pair._3, pair._4).get,
+              vivasDuration
+          )
+        )
+
+        val scheduledVivas = MS01Scheduler
+          .generateScheduledVivas(vivas.toList)
+          .flatMap(scheduleViva => scheduleViva.toOption)
+
+        vivas.zipWithIndex.forall(
+          pair =>
+            scheduledVivas(pair._2).viva.title == pair._1.title && scheduledVivas(
+              pair._2
+            ).viva.student == pair._1.student
+        )
+      }
+    }
+  }
+
   property(
     "after schedule vivas update, all vivas resources should be the same (same id)"
   ) = {
@@ -700,6 +799,124 @@ object Assessment01PropertyBasedTesting extends Properties("") {
           pair =>
             scheduledVivas(pair._2).viva.jury.asResourcesSet == pair._1.jury.asResourcesSet
         )
+      }
+    }
+  }
+
+  property(
+    "after schedule vivas update, all vivas resources should have different availabilities, except the first schedule viva"
+  ) = {
+    val vivasDuration = for {
+      durationPeriod <- Generators.genAtMost24HPeriodOfTime
+      duration <- Duration.create(
+        java.time.Duration.between(durationPeriod._1, durationPeriod._2)
+      )
+    } yield duration
+
+    val dateTimeNow = LocalDateTime.now()
+
+    val vivasToScheduleGenerator = for {
+      duration <- vivasDuration
+      availabilities <- Generators.genAvailabilitySequenceOf(
+        10,
+        dateTimeNow,
+        duration.get.timeDuration
+      )
+      presidents <- Generators.genResourcesWith(
+        availabilities,
+        List(President()),
+        2
+      )
+      advisers <- Generators.genResourcesWith(
+        availabilities,
+        List(Adviser()),
+        2
+      )
+      coAdvisersLength <- Gen.chooseNum(
+        0,
+        Math.min(presidents.length, advisers.length)
+      )
+      supervisorsLength <- Gen.chooseNum(
+        0,
+        Math.min(presidents.length, advisers.length)
+      )
+      coAdvisers <- Gen.listOfN(
+        coAdvisersLength,
+        Generators.genResourcesWith(availabilities, List(CoAdviser()), 0)
+      )
+      supervisors <- Gen.listOfN(
+        supervisorsLength,
+        Generators.genResourcesWith(availabilities, List(Supervisor()), 0)
+      )
+    } yield (duration.get, presidents, advisers, coAdvisers, supervisors)
+
+    Prop.forAll(
+      vivasToScheduleGenerator,
+      Generators.genName,
+      Generators.genName
+    ) { (arguments, student, title) =>
+      {
+
+        val vivasDuration = arguments._1
+
+        val presidents = arguments._2
+
+        val advisers = arguments._3
+
+        val coAdvisers = arguments._4
+
+        val supervisors = arguments._5
+
+        val lessResourcesGeneratedLength =
+          Math.min(presidents.length, advisers.length)
+
+        val resources = (0 until lessResourcesGeneratedLength).map(
+          index =>
+            (
+              presidents(index),
+              advisers(index),
+              supervisors.lift(index).getOrElse(List()),
+              coAdvisers.lift(index).getOrElse(List())
+          )
+        )
+
+        val vivas = resources.map(
+          pair =>
+            Viva.create(
+              student,
+              title,
+              Jury.create(pair._1, pair._2, pair._3, pair._4).get,
+              vivasDuration
+          )
+        )
+
+        val scheduledVivas = MS01Scheduler
+          .generateScheduledVivas(vivas.toList)
+          .flatMap(scheduleViva => scheduleViva.toOption)
+
+        val firstViva = vivas(0)
+
+        val firstScheduleViva = scheduledVivas.headOption.get
+
+        val firstVivaResourcesAvailabilitiesIsEqualToFirstScheduleVivaResourcesAvailabilities = firstViva.jury.asResourcesSet.toList.flatMap(
+          resource => resource.availabilities
+        ) == firstScheduleViva.viva.jury.asResourcesSet.toList
+          .flatMap(resource => resource.availabilities)
+
+        val remainingVivasResourcesAvailabilitiesAreDifferentThanTheScheduleOnes =
+          vivas.tail.zipWithIndex.forall(
+            pair =>
+              pair._1.jury.asResourcesSet.toList
+                .flatMap(resource => resource.availabilities) == scheduledVivas
+                .tail(pair._2)
+                .viva
+                .jury
+                .asResourcesSet
+                .toList
+                .flatMap(resource => resource.availabilities)
+          )
+
+        firstVivaResourcesAvailabilitiesIsEqualToFirstScheduleVivaResourcesAvailabilities && remainingVivasResourcesAvailabilitiesAreDifferentThanTheScheduleOnes
       }
     }
   }
