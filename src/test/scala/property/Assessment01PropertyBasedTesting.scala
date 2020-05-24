57830d44d623f6b61487d054cf511b6da2c82d85
Gustavo Moreira <1130604@isep.ipp.pt>
2020-05-24 16:14:10 +0100

    Cleaned code files from garbage

--- src/test/scala/property/Assessment01PropertyBasedTesting.scala
+++ src/test/scala/property/Assessment01PropertyBasedTesting.scala
@@ -2,181 +2,22 @@ package property
 
 import domain.model.{
   Adviser,
-  Availability,
   CoAdviser,
   Duration,
-  External,
   Jury,
-  NonEmptyString,
-  Period,
-  Preference,
   President,
-  Resource,
-  Role,
   Supervisor,
-  Teacher,
   Viva
 }
 import org.scalacheck.{Gen, Prop, Properties}
-import java.time
 import java.time.LocalDateTime
 
 import assessment.AssessmentMS01
 import domainSchedulerImpl.MS01Scheduler
 import org.scalacheck.Test.Parameters
-import org.scalatest.prop.Configuration
 import xml.Functions
 
 object Assessment01PropertyBasedTesting extends Properties("") {
-
-  /*val genNonEmptyString: Gen[NonEmptyString] = for {
-    s <- Gen.asciiPrintableStr
-    if (!s.isEmpty)
-  } yield NonEmptyString.create(s).get
-
-  val randomPositiveDurations = List[time.Duration](
-    time.Duration.ZERO,
-    time.Duration.ofHours(1),
-    time.Duration.ofHours(2),
-    time.Duration.ofHours(3),
-    time.Duration.ofHours(4)
-  )
-
-  val randomNegativeDurations = List[time.Duration](
-    time.Duration.ofHours(-1),
-    time.Duration.ofHours(-2),
-    time.Duration.ofHours(-3),
-    time.Duration.ofHours(-4)
-  )
-
-  val randomLocalDateTimes = List[List[LocalDateTime]](
-    List(LocalDateTime.now(), LocalDateTime.now().plusHours(1)),
-    List(
-      LocalDateTime.of(2020, 12, 1, 10, 0, 0),
-      LocalDateTime.of(2020, 12, 1, 11, 0, 0)
-    ),
-    List(
-      LocalDateTime.of(2020, 11, 1, 10, 0, 0),
-      LocalDateTime.of(2020, 11, 1, 11, 0, 0)
-    ),
-    List(
-      LocalDateTime.of(2020, 10, 1, 10, 0, 0),
-      LocalDateTime.of(2020, 10, 1, 11, 0, 0)
-    ),
-    List(
-      LocalDateTime.of(2020, 9, 1, 10, 0, 0),
-      LocalDateTime.of(2020, 9, 1, 11, 0, 0)
-    ),
-    List(
-      LocalDateTime.of(2020, 8, 1, 10, 0, 0),
-      LocalDateTime.of(2020, 8, 1, 11, 0, 0)
-    ),
-    List(
-      LocalDateTime.of(2020, 7, 1, 10, 0, 0),
-      LocalDateTime.of(2020, 7, 1, 11, 0, 0)
-    ),
-    List(
-      LocalDateTime.of(2020, 6, 1, 10, 0, 0),
-      LocalDateTime.of(2020, 6, 1, 11, 0, 0)
-    ),
-  )
-
-  val genPositiveDuration: Gen[Duration] = for {
-    d <- Gen.oneOf(randomPositiveDurations)
-  } yield Duration.create(d).get
-
-  val genNegativeDuration: Gen[Duration] = for {
-    d <- Gen.oneOf(randomNegativeDurations)
-  } yield Duration.create(d).get
-
-  val genPeriod: Gen[Period] = for {
-    p <- Gen.oneOf(randomLocalDateTimes)
-  } yield Period.create(p.head, p.last).get
-
-  val genPreference: Gen[Preference] = for {
-    i <- Gen.chooseNum(1, 5)
-  } yield Preference.create(i).get
-
-  val genAvailability: Gen[Availability] = for {
-    period <- genPeriod
-    preference <- genPreference
-  } yield Availability.create(period, preference)
-
-  val genRole: Gen[Role] = for {
-    r <- Gen.oneOf(Set(President(), Adviser(), CoAdviser(), Supervisor()))
-  } yield r
-
-  val genTeacherRole: Gen[Role] = for {
-    r <- Gen.oneOf(Set(President(), Adviser(), CoAdviser()))
-  } yield r
-
-  val genExternalRole: Gen[Role] = for {
-    r <- Gen.oneOf(Set(Supervisor(), CoAdviser()))
-  } yield r
-
-  val genPresidentResource: Gen[Resource] = for {
-    id <- genNonEmptyString
-    name <- genNonEmptyString
-    availabilities <- Gen.listOf(genAvailability)
-  } yield Teacher.create(id, name, availabilities, List(President())).get
-
-  val genAdviserResource: Gen[Resource] = for {
-    id <- genNonEmptyString
-    name <- genNonEmptyString
-    availabilities <- Gen.listOf(genAvailability)
-  } yield Teacher.create(id, name, availabilities, List(Adviser())).get
-
-  val genSupervisorResource: Gen[Resource] = for {
-    id <- genNonEmptyString
-    name <- genNonEmptyString
-    availabilities <- Gen.listOf(genAvailability)
-  } yield Teacher.create(id, name, availabilities, List(Supervisor())).get
-
-  val genCoAdviserResource: Gen[Resource] = for {
-    id <- genNonEmptyString
-    name <- genNonEmptyString
-    availabilities <- Gen.listOf(genAvailability)
-  } yield External.create(id, name, availabilities, List(CoAdviser())).get
-
-  val genTeacherRoles = Gen[List[Role]] = for {
-    d <- Gen.pick(2, List(1,2,3,4))
-  } yield
-
-  val genTeacher: Gen[Teacher] = for {
-    id <- genNonEmptyString
-    name <- genNonEmptyString
-    availabilities <- Gen.listOf(genAvailability)
-    roles <- Gen.pick(2, List(genTeacherRole, genTeacherRole))
-  } yield Teacher.create(id, name, availabilities, roles.map(x => x.sample.get).toList).get
-
-  val genValidExternal: Gen[External] = for {
-    id <- genNonEmptyString
-    name <- genNonEmptyString
-    availabilities <- Gen.listOfN(2, genValidAvailability)
-    roles <- Gen.listOfN(2, genExternalRole)
-  } yield External.create(id, name, availabilities, roles).get
-
-  val genValidResource: Gen[Resource] = for {
-    id <- genNonEmptyString
-    name <- genNonEmptyString
-    availabilities <- Gen.listOf(genValidAvailability)
-    roles <- Gen.listOf(genRole)
-  } yield
-    Resource
-      .validResource(id, name, availabilities, roles)
-      .get
-      .asInstanceOf[Resource]
-
-  val genInvalidResource: Gen[Resource] = for {
-    id <- genNonEmptyString
-    name <- genNonEmptyString
-    availabilities <- Gen.listOf(genInvalidAvailability)
-    roles <- Gen.listOf(genRole)
-  } yield
-    Resource
-      .validResource(id, name, availabilities, roles)
-      .get
-      .asInstanceOf[Resource]*/
 
   override def overrideParameters(p: Parameters): Parameters =
     p.withMinSuccessfulTests(1000)
@@ -501,8 +342,6 @@ object Assessment01PropertyBasedTesting extends Properties("") {
       }
     }
   }
-
-  //property("one resource cannot be overlapped in two scheduled viva") = ???
 
   property(
     "even if vivas take seconds to occur all viva must be scheduled in the time intervals in which its resources are available"
