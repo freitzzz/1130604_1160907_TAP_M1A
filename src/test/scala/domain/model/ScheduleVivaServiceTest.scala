7fc882735635a83e978f2927ec6bc058998f44e7
Gustavo Moreira <1130604@isep.ipp.pt>
2020-06-09 19:25:48 +0100

    Added ScheduleVivaServiceTest file to be available in the branch

---
+++ src/test/scala/domain/model/ScheduleVivaServiceTest.scala
@@ -0,0 +1,11 @@
+package domain.model
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class ScheduleVivaServiceTest extends AnyFunSuite with Matchers {
+
+  test(
+    "the total preference value of a set of resources should be equal to the sum of each resources's individual availabilities preferences"
+  ) {}
+}
