9ce85d21b4a1f57ca28cecb2fc3c64e4b4c22469
João Freitas <1160907@isep.ipp.pt>
2020-06-09 20:22:33 +0100

    Fix merge

--- src/test/scala/domain/model/ScheduleVivaServiceTest.scala
+++ src/test/scala/domain/model/ScheduleVivaServiceTest.scala
@@ -9,36 +9,6 @@ class ScheduleVivaServiceTest extends AnyFunSuite with Matchers {
 
   test(
     "the total preference value of a set of resources should be equal to the sum of each resources's individual availabilities preferences"
-<<<<<<< HEAD
-  ) {}
-
-  test(
-    "if a viva has two resources with two availabilities each of 1 hour and the viva duration is 1 hour, then findPeriodsInWhichVivaCanOccur should return a list with those two availability periods"
-  ) {
-
-    // Arrange
-
-    val dateTimeNow = LocalDateTime.now()
-
-    val periodX = Period.create(dateTimeNow, dateTimeNow.plusHours(1)).get
-
-    val periodY =
-      Period.create(dateTimeNow.plusHours(1), dateTimeNow.plusHours(2)).get
-
-    val preferenceX = Preference.create(5).get
-
-    val preferenceY = Preference.create(5).get
-
-    val availabilityX = Availability.create(periodX, preferenceX)
-
-    val availabilityY = Availability.create(periodY, preferenceY)
-
-    val president = Teacher
-      .create(
-        NonEmptyString.create("T001").get,
-        NonEmptyString.create("President").get,
-        List(availabilityX, availabilityY),
-=======
   ) {
     val presidentAvailabilityStartDateTime = LocalDateTime.now()
 
@@ -77,44 +47,10 @@ class ScheduleVivaServiceTest extends AnyFunSuite with Matchers {
         NonEmptyString.create("1").get,
         NonEmptyString.create("John").get,
         List(presidentAvailability),
->>>>>>> a0e71b707e421d7b1a3937b0b5ee64529744642f
         List(President())
       )
       .get
 
-<<<<<<< HEAD
-    val adviser = Teacher
-      .create(
-        NonEmptyString.create("E001").get,
-        NonEmptyString.create("Adviser").get,
-        List(availabilityX, availabilityY),
-        List(Adviser())
-      )
-      .get
-
-    val jury = Jury.create(president, adviser, List(), List()).get
-
-    val vivaDuration = Duration.create(java.time.Duration.ofHours(1)).get
-
-    val viva = Viva.create(
-      NonEmptyString.create("Student").get,
-      NonEmptyString.create("Functional Programming is cool").get,
-      jury,
-      vivaDuration
-    )
-
-    // Act
-
-    val findPeriodsInWhichVivaCanOccur =
-      ScheduledVivaService.findPeriodsInWhichVivaCanOccur(viva)
-
-    // Assert
-
-    findPeriodsInWhichVivaCanOccur shouldBe List(
-      availabilityX.period,
-      availabilityY.period
-    )
-=======
     val adviser =
       Teacher
         .create(
@@ -146,7 +82,70 @@ class ScheduleVivaServiceTest extends AnyFunSuite with Matchers {
 
     //Assert
     calculatedPreference shouldBe expectedPreference
->>>>>>> a0e71b707e421d7b1a3937b0b5ee64529744642f
+
+  }
+
+  test(
+    "if a viva has two resources with two availabilities each of 1 hour and the viva duration is 1 hour, then findPeriodsInWhichVivaCanOccur should return a list with those two availability periods"
+  ) {
+
+    // Arrange
+
+    val dateTimeNow = LocalDateTime.now()
+
+    val periodX = Period.create(dateTimeNow, dateTimeNow.plusHours(1)).get
+
+    val periodY =
+      Period.create(dateTimeNow.plusHours(1), dateTimeNow.plusHours(2)).get
+
+    val preferenceX = Preference.create(5).get
+
+    val preferenceY = Preference.create(5).get
+
+    val availabilityX = Availability.create(periodX, preferenceX)
+
+    val availabilityY = Availability.create(periodY, preferenceY)
+
+    val president = Teacher
+      .create(
+        NonEmptyString.create("T001").get,
+        NonEmptyString.create("President").get,
+        List(availabilityX, availabilityY),
+        List(President())
+      )
+      .get
+
+    val adviser = Teacher
+      .create(
+        NonEmptyString.create("E001").get,
+        NonEmptyString.create("Adviser").get,
+        List(availabilityX, availabilityY),
+        List(Adviser())
+      )
+      .get
+
+    val jury = Jury.create(president, adviser, List(), List()).get
+
+    val vivaDuration = Duration.create(java.time.Duration.ofHours(1)).get
+
+    val viva = Viva.create(
+      NonEmptyString.create("Student").get,
+      NonEmptyString.create("Functional Programming is cool").get,
+      jury,
+      vivaDuration
+    )
+
+    // Act
+
+    val findPeriodsInWhichVivaCanOccur =
+      ScheduledVivaService.findPeriodsInWhichVivaCanOccur(viva)
+
+    // Assert
+
+    findPeriodsInWhichVivaCanOccur shouldBe List(
+      availabilityX.period,
+      availabilityY.period
+    )
 
   }
 }
