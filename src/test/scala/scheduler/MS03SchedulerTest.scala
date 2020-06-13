7d7a183fdb932e510c3af0ca334ff9f09690f104
Gustavo Moreira <1130604@isep.ipp.pt>
2020-06-13 19:38:15 +0100

    Progress made in MS03SchedulerTests.scala file

--- src/test/scala/scheduler/MS03SchedulerTest.scala
+++ src/test/scala/scheduler/MS03SchedulerTest.scala
@@ -30,39 +30,171 @@ class MS03SchedulerTest extends AnyFunSuite with Matchers {
 
   test(
     "given a list of vivas that do not share resources, then the schedule that maximizes schedule preference of vivas which do not share resources is applied"
-  ) {}
+  ) {
+
+    //Arrange
+
+    val vivas = VivasGiver.giveMeAListOfVivasThatDoNotShareResources()
+
+    //Act
+
+    val scheduledVivas = MS03Scheduler.scheduleVivas(vivas)
+    val expectedScheduledVivasSize = vivas.size
+
+    //Assert
+
+    scheduledVivas.size shouldBe expectedScheduledVivasSize
+    scheduledVivas
+      .filter(_.isSuccess)
+      .map(x => x.get)
+      .map(x => x.scheduledPreference)
+      .sum shouldBe 25
+  }
 
   test(
     "given a list of vivas that only share resources, then the schedule that maximizes schedule preference of vivas which that share resources is applied"
-  ) {}
+  ) {
+
+    //Arrange
+
+    val vivas = VivasGiver.giveMeAListOfVivasThatOnlyShareResources()
+
+    //Act
+
+    val scheduledVivas = MS03Scheduler.scheduleVivas(vivas)
+    val expectedScheduledVivasSize = vivas.size
+
+    //Assert
+
+    scheduledVivas.size shouldBe expectedScheduledVivasSize
+    scheduledVivas
+      .filter(_.isSuccess)
+      .map(x => x.get)
+      .map(x => x.scheduledPreference)
+      .sum shouldBe 21
+  }
 
   test(
     "given a list of vivas that share and don't resources, then the schedule that maximizes schedule preference of vivas which that share resources is applied to vivas which share resources and the schedule that maximizes schedule preference of vivas which do not share resources is applied to vivas which do not share resources"
-  ) {}
+  ) {
+
+    //Arrange
+
+    val vivas = VivasGiver.giveMeAListOfVivasThatShareResources()
+
+    //Act
+
+    val scheduledVivas = MS03Scheduler.scheduleVivas(vivas)
+    val expectedScheduledVivasSize = vivas.size
+
+    //Assert
+    scheduledVivas.size shouldBe expectedScheduledVivasSize
+    scheduledVivas
+      .filter(_.isSuccess)
+      .map(x => x.get)
+      .map(x => x.scheduledPreference)
+      .sum shouldBe 36
+  }
 
   test(
     "given a list of vivas which schedule period is not the same, then scheduler returns the scheduled vivas based on their period earliness"
-  ) {}
+  ) {
+
+    //Arrange
+
+    val vivas = VivasGiver.giveMeAListOfVivasThatDoNotShareTimePeriods()
+
+    //Act
+    val scheduledVivas = MS03Scheduler.scheduleVivas(vivas)
+    val expectedScheduledVivasSize = vivas.size
+    val scheduledVivasPeriods = scheduledVivas.map(x => x.get.period)
+
+    //Assert
+    scheduledVivas.size shouldBe expectedScheduledVivasSize
+    scheduledVivas
+      .filter(_.isSuccess)
+      .map(x => x.get)
+      .map(x => x.scheduledPreference)
+      .sum shouldBe 25
+
+    for (a <- scheduledVivasPeriods; b <- scheduledVivasPeriods) {
+      a.start.isBefore(b.start)
+      a.end.isBefore(b.end)
+    }
+  }
 
   test(
     "given a list of vivas which schedule period is the same, then scheduler returns the scheduled vivas based on their period earliness and to those scheduled vivas which period is the same the order is based on the viva title"
-  ) {}
+  ) {
+
+    //Arrange
+    val vivas = VivasGiver.giveMeAListOfVivasThatShareTheExactSameTimePeriod()
+
+    //Act
+    val scheduledVivas = MS03Scheduler.scheduleVivas(vivas)
+    val expectedScheduledVivasSize = vivas.size
+    val scheduledVivasPeriods = scheduledVivas.map(x => x.get.period)
+    val scheduledVivasTitles = scheduledVivas.map(x => x.get.viva.title.s)
+
+    //Assert
+    scheduledVivas.size shouldBe expectedScheduledVivasSize
+    scheduledVivas
+      .filter(_.isSuccess)
+      .map(x => x.get)
+      .map(x => x.scheduledPreference)
+      .sum shouldBe 23
+
+    for (a <- scheduledVivasPeriods; b <- scheduledVivasPeriods) {
+      a.start.isEqual(b.start)
+      a.end.isEqual(b.end)
+    }
+
+    scheduledVivasTitles shouldEqual vivas.map(x => x.title.s)
+  }
 
   test(
     "given a list of vivas which resources have availabilities that overlap after a period division, but at least a combination of scheduled vivas is successful, then the scheduler returns a successful complete schedule"
-  ) {}
+  ) {
+
+    //Arrange
+
+    //Act
+
+    //Assert
+  }
 
   test(
     "given a list of vivas that shares resources and it was not possible to schedule these then the scheduler returns a Failure indicating that the schedule is impossible"
-  ) {}
+  ) {
+
+    //Arrange
+
+    //Act
+
+    //Assert
+  }
 
   test(
     "given a list of vivas in which two vivas share the same jury, then the combination of scheduled vivas is based on the max sum of scheduled preferences and based on the order of the input of these two vivas that share the same jury"
-  ) {}
+  ) {
+
+    //Arrange
+
+    //Act
+
+    //Assert
+  }
 
   test(
     "given a list of vivas in which four or more (6/8/10 ...) vivas share the same jury, then the combination of scheduled vivas is based on the max sum of scheduled preferences and based on the order of the input of these vivas that share the same jury"
-  ) {}
+  ) {
+
+    //Arrange
+
+    //Act
+
+    //Assert
+  }
 
   object VivasGiver {
 
@@ -199,6 +331,106 @@ class MS03SchedulerTest extends AnyFunSuite with Matchers {
               <external id="E002" name="External 002">
                 <availability start="2020-05-30T09:30:00" end="2020-05-30T10:30:00" preference="5"/>
                 <availability start="2020-05-30T11:30:00" end="2020-05-30T12:30:00" preference="4"/>
+              </external>
+            </externals>
+          </resources>
+        </agenda>
+
+      Functions.deserialize(xml).get
+
+    }
+
+    def giveMeAListOfVivasThatDoNotShareTimePeriods(): List[Viva] = {
+
+      val xml =
+        <agenda xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../agenda.xsd"
+                duration="01:00:00">
+          <vivas>
+            <viva student="Student 001" title="Title 1">
+              <president id="T001"/>
+              <adviser id="T002"/>
+              <supervisor id="E001"/>
+            </viva>
+            <viva student="Student 002" title="Title 2">
+              <president id="T003"/>
+              <adviser id="T004"/>
+              <supervisor id="E002"/>
+            </viva>
+          </vivas>
+          <resources>
+            <teachers>
+              <teacher id="T001" name="Teacher 001">
+                <availability start="2020-05-30T09:30:00" end="2020-05-30T12:30:00" preference="5"/>
+                <availability start="2020-05-30T13:30:00" end="2020-05-30T16:30:00" preference="3"/>
+              </teacher>
+              <teacher id="T002" name="Teacher 002">
+                <availability start="2020-05-30T10:30:00" end="2020-05-30T11:30:00" preference="5"/>
+                <availability start="2020-05-30T14:30:00" end="2020-05-30T17:00:00" preference="5"/>
+              </teacher>
+              <teacher id="T003" name="Teacher 003">
+                <availability start="2020-05-30T09:30:00" end="2020-05-30T12:30:00" preference="5"/>
+                <availability start="2020-05-30T13:30:00" end="2020-05-30T16:30:00" preference="3"/>
+              </teacher>
+              <teacher id="T004" name="Teacher 004">
+                <availability start="2020-05-30T10:30:00" end="2020-05-30T11:30:00" preference="5"/>
+                <availability start="2020-05-30T14:30:00" end="2020-05-30T17:00:00" preference="5"/>
+              </teacher>
+            </teachers>
+            <externals>
+              <external id="E001" name="External 001">
+                <availability start="2020-05-30T10:00:00" end="2020-05-30T13:30:00" preference="2"/>
+                <availability start="2020-05-30T15:30:00" end="2020-05-30T18:00:00" preference="5"/>
+              </external>
+              <external id="E002" name="External 002">
+                <availability start="2020-05-30T10:00:00" end="2020-05-30T13:30:00" preference="1"/>
+                <availability start="2020-05-30T14:00:00" end="2020-05-30T18:30:00" preference="4"/>
+              </external>
+            </externals>
+          </resources>
+        </agenda>
+
+      Functions.deserialize(xml).get
+
+    }
+
+    def giveMeAListOfVivasThatShareTheExactSameTimePeriod(): List[Viva] = {
+
+      val xml =
+        <agenda xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../agenda.xsd"
+                duration="01:00:00">
+          <vivas>
+            <viva student="Student 001" title="Title 1">
+              <president id="T001"/>
+              <adviser id="T002"/>
+              <supervisor id="E001"/>
+            </viva>
+            <viva student="Student 002" title="Title 2">
+              <president id="T003"/>
+              <adviser id="T004"/>
+              <supervisor id="E002"/>
+            </viva>
+          </vivas>
+          <resources>
+            <teachers>
+              <teacher id="T001" name="Teacher 001">
+                <availability start="2020-05-30T09:30:00" end="2020-05-30T12:30:00" preference="5"/>
+              </teacher>
+              <teacher id="T002" name="Teacher 002">
+                <availability start="2020-05-30T10:30:00" end="2020-05-30T11:30:00" preference="5"/>
+              </teacher>
+              <teacher id="T003" name="Teacher 003">
+                <availability start="2020-05-30T09:30:00" end="2020-05-30T12:30:00" preference="5"/>
+              </teacher>
+              <teacher id="T004" name="Teacher 004">
+                <availability start="2020-05-30T10:30:00" end="2020-05-30T11:30:00" preference="5"/>
+              </teacher>
+            </teachers>
+            <externals>
+              <external id="E001" name="External 001">
+                <availability start="2020-05-30T10:00:00" end="2020-05-30T13:30:00" preference="2"/>
+              </external>
+              <external id="E002" name="External 002">
+                <availability start="2020-05-30T10:00:00" end="2020-05-30T13:30:00" preference="1"/>
               </external>
             </externals>
           </resources>
