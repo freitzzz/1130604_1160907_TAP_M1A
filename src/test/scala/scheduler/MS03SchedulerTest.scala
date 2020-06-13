235c72e881b0c3aebffe82e828c632e85e093d53
Gustavo Moreira <1130604@isep.ipp.pt>
2020-06-13 19:57:27 +0100

    Added more unit tests for the MS03Scheduler

--- src/test/scala/scheduler/MS03SchedulerTest.scala
+++ src/test/scala/scheduler/MS03SchedulerTest.scala
@@ -154,14 +154,7 @@ class MS03SchedulerTest extends AnyFunSuite with Matchers {
 
   test(
     "given a list of vivas which resources have availabilities that overlap after a period division, but at least a combination of scheduled vivas is successful, then the scheduler returns a successful complete schedule"
-  ) {
-
-    //Arrange
-
-    //Act
-
-    //Assert
-  }
+  ) {}
 
   test(
     "given a list of vivas that shares resources and it was not possible to schedule these then the scheduler returns a Failure indicating that the schedule is impossible"
@@ -169,31 +162,61 @@ class MS03SchedulerTest extends AnyFunSuite with Matchers {
 
     //Arrange
 
+    val vivas = VivasGiver.giveMeAListOfVivasThatCannotBeFullyScheduled()
+
     //Act
+    val scheduledVivas = MS03Scheduler.scheduleVivas(vivas)
 
     //Assert
+    scheduledVivas.head.isFailure shouldBe true
   }
 
   test(
     "given a list of vivas in which two vivas share the same jury, then the combination of scheduled vivas is based on the max sum of scheduled preferences and based on the order of the input of these two vivas that share the same jury"
   ) {
 
-    //Arrange
+    val vivas = VivasGiver.giveMeAListOfVivasThatOnlyShareResources()
 
     //Act
 
+    val scheduledVivas = MS03Scheduler.scheduleVivas(vivas)
+    val expectedScheduledVivasSize = vivas.size
+    val expectedScheduledVivasOrderTitles = vivas.map(x => x.title.s)
+
     //Assert
+
+    scheduledVivas.size shouldBe expectedScheduledVivasSize
+    scheduledVivas
+      .filter(_.isSuccess)
+      .map(x => x.get)
+      .map(x => x.scheduledPreference)
+      .sum shouldBe 21
+
+    scheduledVivas.map(x => x.get.viva.title.s) shouldEqual expectedScheduledVivasOrderTitles
   }
 
   test(
     "given a list of vivas in which four or more (6/8/10 ...) vivas share the same jury, then the combination of scheduled vivas is based on the max sum of scheduled preferences and based on the order of the input of these vivas that share the same jury"
   ) {
 
-    //Arrange
+    val vivas = VivasGiver.giveMeAListOf6VivasThatShareTheSameResources()
 
     //Act
 
+    val scheduledVivas = MS03Scheduler.scheduleVivas(vivas)
+    val expectedScheduledVivasSize = vivas.size
+    val expectedScheduledVivasOrderTitles = vivas.map(x => x.title.s)
+
     //Assert
+
+    scheduledVivas.size shouldBe expectedScheduledVivasSize
+    scheduledVivas
+      .filter(_.isSuccess)
+      .map(x => x.get)
+      .map(x => x.scheduledPreference)
+      .sum shouldBe 40
+
+    scheduledVivas.map(x => x.get.viva.title.s) shouldEqual expectedScheduledVivasOrderTitles
   }
 
   object VivasGiver {
@@ -432,6 +455,106 @@ class MS03SchedulerTest extends AnyFunSuite with Matchers {
               <external id="E002" name="External 002">
                 <availability start="2020-05-30T10:00:00" end="2020-05-30T13:30:00" preference="1"/>
               </external>
+            </externals>
+          </resources>
+        </agenda>
+
+      Functions.deserialize(xml).get
+
+    }
+
+    def giveMeAListOfVivasThatCannotBeFullyScheduled(): List[Viva] = {
+
+      val xml =
+        <agenda xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../agenda.xsd"
+                duration="01:00:00">
+          <vivas>
+            <viva student="Student 001" title="Title 1">
+              <president id="T001"/>
+              <adviser id="T002"/>
+              <supervisor id="E001"/>
+            </viva>
+            <viva student="Student 002" title="Title 2">
+              <president id="T002"/>
+              <adviser id="T001"/>
+              <supervisor id="E001"/>
+            </viva>
+          </vivas>
+          <resources>
+            <teachers>
+              <teacher id="T001" name="Teacher 001">
+                <availability start="2020-05-30T09:30:00" end="2020-05-30T12:30:00" preference="5"/>
+              </teacher>
+              <teacher id="T002" name="Teacher 002">
+                <availability start="2020-05-30T10:30:00" end="2020-05-30T11:30:00" preference="5"/>
+                <availability start="2020-05-30T14:30:00" end="2020-05-30T17:00:00" preference="5"/>
+              </teacher>
+            </teachers>
+            <externals>
+              <external id="E001" name="External 001">
+                <availability start="2020-05-30T10:00:00" end="2020-05-30T13:30:00" preference="2"/>
+                <availability start="2020-05-30T15:30:00" end="2020-05-30T18:00:00" preference="5"/>
+              </external>
+              <external id="E002" name="External 002">
+                <availability start="2020-05-30T10:00:00" end="2020-05-30T13:30:00" preference="1"/>
+              </external>
+            </externals>
+          </resources>
+        </agenda>
+
+      Functions.deserialize(xml).get
+
+    }
+
+    def giveMeAListOf6VivasThatShareTheSameResources(): List[Viva] = {
+
+      val xml =
+        <agenda xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../agenda.xsd"
+                duration="01:00:00">
+          <vivas>
+            <viva student="Student 001" title="Title 1">
+              <president id="T001"/>
+              <adviser id="T002"/>
+            </viva>
+            <viva student="Student 002" title="Title 2">
+              <president id="T002"/>
+              <adviser id="T001"/>
+            </viva>
+            <viva student="Student 003" title="Title 3">
+              <president id="T001"/>
+              <adviser id="T002"/>
+            </viva>
+            <viva student="Student 004" title="Title 4">
+              <president id="T001"/>
+              <adviser id="T002"/>
+            </viva>
+            <viva student="Student 005" title="Title 5">
+              <president id="T001"/>
+              <adviser id="T002"/>
+            </viva>
+            <viva student="Student 006" title="Title 6">
+              <president id="T001"/>
+              <adviser id="T002"/>
+            </viva>
+          </vivas>
+          <resources>
+            <teachers>
+              <teacher id="T001" name="Teacher 001">
+                <availability start="2020-05-30T09:30:00" end="2020-05-30T10:30:00" preference="5"/>
+                <availability start="2020-05-30T14:30:00" end="2020-05-30T15:30:00" preference="3"/>
+                <availability start="2020-05-30T18:30:00" end="2020-05-30T20:30:00" preference="4"/>
+                <availability start="2020-05-31T10:30:00" end="2020-05-31T13:30:00" preference="2"/>
+                <availability start="2020-05-31T15:30:00" end="2020-05-31T17:30:00" preference="1"/>
+              </teacher>
+              <teacher id="T002" name="Teacher 002">
+                <availability start="2020-05-30T09:30:00" end="2020-05-30T10:30:00" preference="5"/>
+                <availability start="2020-05-30T14:30:00" end="2020-05-30T15:30:00" preference="3"/>
+                <availability start="2020-05-30T18:30:00" end="2020-05-30T20:30:00" preference="4"/>
+                <availability start="2020-05-31T10:30:00" end="2020-05-31T13:30:00" preference="2"/>
+                <availability start="2020-05-31T15:30:00" end="2020-05-31T17:30:00" preference="1"/>
+              </teacher>
+            </teachers>
+            <externals>
             </externals>
           </resources>
         </agenda>
